8ce5838d4a7bf096d73cc49cfa841a65c8ef41ff
Aakash Saini <sainiaakash177@gmail.com>
2025-11-24 15:14:15 +0530

    fix: added PORT for Render deployment

--- server/index.js
+++ server/index.js
@@ -333,6 +333,9 @@ app.get("/", (req, res) => {
   res.send("Resume Parser API Running");
 });
 
-app.listen(3000, () =>
-  console.log("Server running on http://localhost:3000")
-);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
+
