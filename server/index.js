a25b7fb0aa9ea23dbb24a1b6f0b55be7377c09d6
Aakash Saini <sainiaakash177@gmail.com>
2025-12-03 10:55:10 +0530

    change port

--- server/index.js
+++ server/index.js
@@ -52,5 +52,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
